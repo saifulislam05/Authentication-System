6a947018e9bf3f1774c544b35b4a14b2d61d376e
saifulislam05 <saiful.bijulia@gmail.com>
2024-01-23 16:27:27 +0530

    fixed issues

--- src/Firebase/firebaseConfig.jsx
+++ src/Firebase/firebaseConfig.jsx
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD0bhUCigWGEMVrjr5YtAHvSIiw7c1eP0A",
-  authDomain: "geekster-auth.firebaseapp.com",
-  projectId: "geekster-auth",
-  storageBucket: "geekster-auth.appspot.com",
-  messagingSenderId: "130633062856",
-  appId: "1:130633062856:web:14eb8cbd845c527c3048fd",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_API_ID,
 };
 
 // Initialize Firebase
